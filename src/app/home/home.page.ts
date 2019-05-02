412683e7d67cbbafd52a26e95d48065c6111d0f6
elsawy <m.elsawy1996@gmail.com>
2019-05-02 15:47:16 +0200

    add function add emp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -21,44 +21,11 @@ export class HomePage implements OnInit{
     public alertController: AlertController,
     private device: Device,
     private fb: FormBuilder,
-  ) { 
-
-  }
-  formName: FormGroup;
-  // onKeydownEvent(event){
-  //   //console.log(event.target.name);
-
-  //   //console.log(event.target.textLength)
-  //   console.log(event.srcElement.selectionEnd)
-
-  //   if(event.srcElement.selectionEnd <= 0){
-  //     console.log(event.target.name)
-  //   }
-  // }
-  model: any = {};
-  invalid:any = '';
-  onSubmit() {
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.employeeDetails))
-  }
+  ) { }
   ngOnInit() {
- 
-    // alert('this.device.cordova : ' + this.device.cordova);
-    // alert('this.device.isVirtual : ' + this.device.isVirtual);
-    // alert('this.device.manufacturer : ' + this.device.manufacturer);
-    // alert('this.device.model : ' + this.device.model);
-    // alert('this.device.platform : ' + this.device.platform);
-    // alert('this.device.serial : ' + this.device.serial);
-     //alert('this.device.uuid' + this.device.uuid);
-    // alert('this.device.version' + this.device.version)
-
-  //   this.uniqueDeviceID.get()
-  // .then((uuid: any) => alert(uuid))
-  // .catch((error: any) => alert(error));
   }
   async addEmployee(dataEmployee) {
- 
     this.restApi.createEmployee(this.employeeDetails).subscribe(async (data: {}) => {
-     
       console.log(data)
       const alert = await this.alertController.create({
         header: 'Alert',
@@ -66,7 +33,6 @@ export class HomePage implements OnInit{
         message: 'This is an alert message.',
         buttons: ['OK']
       });
-  
        await alert.present();
        this.router.navigate(['/list']);
       
